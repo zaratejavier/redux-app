cdf0d386ccac22419ac9425b700b2c791fcd7130
Javier Zarate <zarate.javier.jz@gmail.com>
2020-07-20 10:21:46 -0600

    added todo component

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,5 +1,17 @@
 const todos = (state = [], action) => {
-  return state
+  switch (action.type) {
+    case 'TODOS':
+      return action.todos;
+    case 'APP_TODO':
+      return [...state, action.todo]
+    case "TOGGLE_TODO":
+      return state.map(todo => {
+        if (todo.id === action.id) return { ...todo, complete: todo.complete }
+        return todo
+      })
+    default:
+      return state;
+  }
 }
 
 export default todos;
